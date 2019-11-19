5d726a0251e81e72c93ace70d051cea80a811e2e
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-11-19 01:22:21 -0300

    adiciona novos metodos

--- firebaseauth_wrapper/lib/firebaseauth_wrapper.dart
+++ firebaseauth_wrapper/lib/firebaseauth_wrapper.dart
@@ -1,5 +1,21 @@
-abstract class FirebaseAuth{
+abstract class UserCredential {}
+
+abstract class ActionCodeSettings {}
+
+abstract class ActionCodeInfo {}
+
+abstract class FirebaseAuth {
   Stream<String> get onUserIdChange;
   Future<bool> loginWithEmailAndPassword(String email, String password);
   Future logout();
+
+  Future<UserCredential> createUserWithEmailAndPassword(
+      String email, String password);
+
+  Future sendPasswordResetEmail(String email,
+      [ActionCodeSettings actionCodeSettings]);
+  Future confirmPasswordReset(String code, String password);
+  Future<String> verifyPasswordResetCode(String code);
+  Future<ActionCodeInfo> checkActionCode(String code);
+  Future applyActionCode(String code);
 }
